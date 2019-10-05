b47a6fde74f5040d4441a54cf3d7ccaeecd31201
David Tolnay <dtolnay@gmail.com>
2019-10-04 23:14:09 -0400

    Drop the dependency on a macros crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,25 +4,6 @@ mod as_error;
 mod exception;
 mod context;
 
-#[doc(inline)]
-/// Annotations a function that "throws" a Result.
-///
-/// Inside functions tagged with `throws`, you can use `?` and the `throw!` macro to return errors,
-/// but you don't need to wrap the successful return values in `Ok`.
-///
-/// `throws` can optionally take a type as an argument, which will be the error type returned by
-/// this function. By default, the function will throw `Exception`.
-pub use fehler_macros::throws;
-
-#[doc(inline)]
-/// Derive for the Error trait.
-///
-/// If the type contains a `Backtrace`, it will generate the `backtrace` method to find it.
-///
-/// If this wraps a different, underlying error, you can tag that field with `#[error::source]`,
-/// which will generate the `cause` and `source` methods correctly.
-pub use fehler_macros::Error;
-
 pub use crate::as_error::AsError;
 pub use crate::exception::{Exception, Errors};
 pub use crate::context::Context;
