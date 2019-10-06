5bbb899cc62522be7e43311474ff0b0608cd9891
David Tolnay <dtolnay@gmail.com>
2019-10-06 13:10:02 -0400

    Document bail macro with examples

--- src/lib.rs
+++ src/lib.rs
@@ -219,6 +219,64 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// Return early with an error.
 ///
 /// This macro is equivalent to `return Err(From::from($err))`.
+///
+/// # Example
+///
+/// ```
+/// # use anyhow::{bail, Result};
+/// #
+/// # fn has_permission(user: usize, resource: usize) -> bool {
+/// #     true
+/// # }
+/// #
+/// # fn main() -> Result<()> {
+/// #     let user = 0;
+/// #     let resource = 0;
+/// #
+/// if !has_permission(user, resource) {
+///     bail!("permission denied for accessing {}", resource);
+/// }
+/// #     Ok(())
+/// # }
+/// ```
+///
+/// ```
+/// # use anyhow::{bail, Result};
+/// # use std::fmt::{self, Display};
+/// #
+/// # #[derive(Debug)]
+/// # enum ScienceError {
+/// #     RecursionLimitExceeded,
+/// # }
+/// #
+/// # impl std::error::Error for ScienceError {}
+/// #
+/// # impl Display for ScienceError {
+/// #     fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+/// #         unimplemented!()
+/// #     }
+/// # }
+/// #
+/// # const MAX_DEPTH: usize = 1;
+/// #
+/// # const IGNORE: &str = stringify! {
+/// #[derive(Error, Debug)]
+/// enum ScienceError {
+///     #[error(display = "recursion limit exceeded")]
+///     RecursionLimitExceeded,
+///     ...
+/// }
+/// # };
+///
+/// # fn main() -> Result<()> {
+/// #     let depth = 0;
+/// #
+/// if depth > MAX_DEPTH {
+///     bail!(ScienceError::RecursionLimitExceeded);
+/// }
+/// #     Ok(())
+/// # }
+/// ```
 #[macro_export]
 macro_rules! bail {
     ($err:expr $(,)?) => {
