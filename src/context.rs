bab03c184014304c4b87d8b38ba2a59bee346630
David Tolnay <dtolnay@gmail.com>
2019-10-06 15:41:47 -0400

    Document Context trait with example

--- src/context.rs
+++ src/context.rs
@@ -6,13 +6,55 @@ use std::fmt::{self, Debug, Display};
 use std::backtrace::Backtrace;
 
 /// Provides the `context` method for `Result`.
+///
+/// # Example
+///
+/// ```
+/// use anyhow::{Context, Result};
+/// use std::fs;
+/// use std::path::PathBuf;
+///
+/// pub struct ImportantThing {
+///     path: PathBuf,
+/// }
+///
+/// impl ImportantThing {
+///     # const IGNORE: &'static str = stringify! {
+///     pub fn detach(&mut self) -> Result<()> {...}
+///     # };
+///     # fn detach(&mut self) -> Result<()> {
+///     #     unimplemented!()
+///     # }
+/// }
+///
+/// pub fn do_it(mut it: ImportantThing) -> Result<Vec<u8>> {
+///     it.detach().context("failed to detach the important thing")?;
+///
+///     let path = &it.path;
+///     let content = fs::read(path)
+///         .with_context(|| format!("failed to read instrs from {}", path.display()))?;
+///
+///     Ok(content)
+/// }
+/// ```
+///
+/// When printed, the outermost context would be printed first and the lower
+/// level underlying causes would be enumerated below.
+///
+/// ```console
+/// Error: failed to read instrs from ./path/to/instrs.jsox
+///
+/// caused by:
+///     0: No such file or directory (os error 2)
+/// ```
 pub trait Context<T, E> {
     /// Wrap the error value with additional context.
     fn context<C>(self, context: C) -> Result<T, Error>
     where
         C: Display + Send + Sync + 'static;
 
-    /// Wrap the error value with additional context lazily.
+    /// Wrap the error value with additional context that is evaluated lazily
+    /// only once an error does occur.
     fn with_context<C, F>(self, f: F) -> Result<T, Error>
     where
         C: Display + Send + Sync + 'static,
