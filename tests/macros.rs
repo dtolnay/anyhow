dd1876cb320db35481f13761e3531ad855a6a7c0
David Tolnay <dtolnay@gmail.com>
2019-10-07 13:43:36 -0700

    Move macro tests to tests directory

--- tests/macros.rs
+++ tests/macros.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Result};
+use anyhow::{bail, ensure, Result};
 use std::io;
 
 fn bail_literal() -> Result<()> {
@@ -25,4 +25,26 @@ fn test_downcast() {
     assert_eq!("oh no!", bail_literal().unwrap_err().downcast::<&str>().unwrap());
     assert_eq!("oh no!", bail_fmt().unwrap_err().downcast::<String>().unwrap());
     assert_eq!("oh no!", bail_error().unwrap_err().downcast::<io::Error>().unwrap().to_string());
+}
+
+#[test]
+fn test_ensure() {
+    let f = || {
+        ensure!(1 + 1 == 2, "This is correct");
+        Ok(())
+    };
+    assert!(f().is_ok());
+
+    let v = 1;
+    let f = || {
+        ensure!(v + v == 2, "This is correct, v: {}", v);
+        Ok(())
+    };
+    assert!(f().is_ok());
+
+    let f = || {
+        ensure!(v + v == 1, "This is not correct, v: {}", v);
+        Ok(())
+    };
+    assert!(f().is_err());
 }
