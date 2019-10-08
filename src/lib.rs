cc6501e665e3c887896ad3c947e8bdecf34be545
David Tolnay <dtolnay@gmail.com>
2019-10-08 08:18:40 -0700

    Release 1.0.4

--- src/lib.rs
+++ src/lib.rs
@@ -171,7 +171,7 @@
 //!
 //! [fehler]: https://github.com/withoutboats/fehler
 
-#![doc(html_root_url = "https://docs.rs/anyhow/1.0.3")]
+#![doc(html_root_url = "https://docs.rs/anyhow/1.0.4")]
 #![cfg_attr(backtrace, feature(backtrace))]
 
 #[macro_use]
