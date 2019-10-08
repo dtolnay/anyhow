29f9375aee41775c2cc4d90caf741b4d5b7f1d1f
David Tolnay <dtolnay@gmail.com>
2019-10-08 00:49:02 -0700

    Format with rustfmt 2019-09-08

--- tests/macros.rs
+++ tests/macros.rs
@@ -22,9 +22,22 @@ fn test_messages() {
 
 #[test]
 fn test_downcast() {
-    assert_eq!("oh no!", bail_literal().unwrap_err().downcast::<&str>().unwrap());
-    assert_eq!("oh no!", bail_fmt().unwrap_err().downcast::<String>().unwrap());
-    assert_eq!("oh no!", bail_error().unwrap_err().downcast::<io::Error>().unwrap().to_string());
+    assert_eq!(
+        "oh no!",
+        bail_literal().unwrap_err().downcast::<&str>().unwrap(),
+    );
+    assert_eq!(
+        "oh no!",
+        bail_fmt().unwrap_err().downcast::<String>().unwrap(),
+    );
+    assert_eq!(
+        "oh no!",
+        bail_error()
+            .unwrap_err()
+            .downcast::<io::Error>()
+            .unwrap()
+            .to_string(),
+    );
 }
 
 #[test]
