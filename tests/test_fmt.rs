a2b3fbd41add65e83c3b9fdc62bc020901b8b102
David Tolnay <dtolnay@gmail.com>
2019-11-15 21:34:36 -0800

    Line up altdisplay expected strings on one line
    Rustfmt does not like how these were previously formatted and insisted
    on doing:
    
        const EXPECTED_ALTDISPLAY_G: &str = "\
                                             f failed: oh no!\
                                             ";
    
    which is worse.

--- tests/test_fmt.rs
+++ tests/test_fmt.rs
@@ -13,17 +13,11 @@ fn h() -> Result<()> {
     g().context("g failed")
 }
 
-const EXPECTED_ALTDISPLAY_F: &str = "\
-oh no!\
-";
+const EXPECTED_ALTDISPLAY_F: &str = "oh no!";
 
-const EXPECTED_ALTDISPLAY_G: &str = "\
-f failed: oh no!\
-";
+const EXPECTED_ALTDISPLAY_G: &str = "f failed: oh no!";
 
-const EXPECTED_ALTDISPLAY_H: &str = "\
-g failed: f failed: oh no!\
-";
+const EXPECTED_ALTDISPLAY_H: &str = "g failed: f failed: oh no!";
 
 const EXPECTED_DEBUG_F: &str = "\
 oh no!
