7246344a3e66c652f08cc7efef97555844810d58
David Tolnay <dtolnay@gmail.com>
2019-11-17 14:40:56 -0800

    Remove unneeded take in Chain implementation

--- src/error.rs
+++ src/error.rs
@@ -731,7 +731,7 @@ impl<'a> Iterator for Chain<'a> {
     type Item = &'a (dyn StdError + 'static);
 
     fn next(&mut self) -> Option<Self::Item> {
-        let next = self.next.take()?;
+        let next = self.next?;
         self.next = next.source();
         Some(next)
     }
