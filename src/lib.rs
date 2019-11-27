d175627ab6a6126bb56af9495464ee5040c81ac1
David Tolnay <dtolnay@gmail.com>
2019-11-27 14:55:28 -0800

    Release 1.0.24

--- src/lib.rs
+++ src/lib.rs
@@ -159,7 +159,7 @@
 //!   # }
 //!   ```
 
-#![doc(html_root_url = "https://docs.rs/anyhow/1.0.23")]
+#![doc(html_root_url = "https://docs.rs/anyhow/1.0.24")]
 #![cfg_attr(backtrace, feature(backtrace))]
 #![allow(
     clippy::needless_doctest_main,
