a1b8cbd0f1d73bd80c6c9b0d37cd88d0f0d851e1
Jane Lusby <jlusby@yaah.dev>
2019-12-11 09:17:22 -0800

    Apply indent in causes on each new line

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,6 +1,7 @@
 use crate::chain::Chain;
 use crate::error::ErrorImpl;
 use core::fmt::{self, Debug};
+use std::fmt::Write;
 
 impl ErrorImpl<()> {
     pub(crate) fn display(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -25,14 +26,15 @@ impl ErrorImpl<()> {
         write!(f, "{}", error)?;
 
         if let Some(cause) = error.source() {
-            write!(f, "\n\nCaused by:")?;
+            writeln!(f, "\n\nCaused by:")?;
             let multiple = cause.source().is_some();
             for (n, error) in Chain::new(cause).enumerate() {
-                write!(f, "\n    ")?;
-                if multiple {
-                    write!(f, "{}: ", n)?;
-                }
-                write!(f, "{}", error)?;
+                let mut f = Numbered {
+                    inner: &mut *f,
+                    ind: Some(n).filter(|_| multiple),
+                    started: false,
+                };
+                writeln!(f, "{}", error)?;
             }
         }
 
@@ -53,5 +55,109 @@ impl ErrorImpl<()> {
         }
 
         Ok(())
+    }
+}
+
+struct Numbered<D> {
+    inner: D,
+    ind: Option<usize>,
+    started: bool,
+}
+
+impl<T> fmt::Write for Numbered<T>
+where
+    T: fmt::Write,
+{
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        if !self.started {
+            if let Some(ind) = &self.ind {
+                self.inner.write_fmt(format_args!("{: >4}: ", ind))?;
+            } else {
+                self.inner.write_str("    ")?;
+            }
+            self.started = true;
+        }
+
+        s.chars().try_for_each(|c| {
+            self.inner.write_char(c)?;
+
+            if c == '\n' {
+                if self.ind.is_some() {
+                    self.inner.write_str("      ")?;
+                } else {
+                    self.inner.write_str("    ")?;
+                }
+            }
+
+            Ok(())
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_digit() {
+        let input = "verify\nthis";
+        let expected = "   2: verify\n      this";
+        let mut output = String::new();
+
+        Numbered {
+            inner: &mut output,
+            ind: Some(2),
+            started: false,
+        }
+        .write_str(input)
+        .unwrap();
+
+        println!("{}", input);
+        println!("{}", output);
+        println!("{}", expected);
+
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn two_digits() {
+        let input = "verify\nthis";
+        let expected = "  12: verify\n      this";
+        let mut output = String::new();
+
+        Numbered {
+            inner: &mut output,
+            ind: Some(12),
+            started: false,
+        }
+        .write_str(input)
+        .unwrap();
+
+        println!("{}", input);
+        println!("{}", output);
+        println!("{}", expected);
+
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn no_digits() {
+        let input = "verify\nthis";
+        let expected = "    verify\n    this";
+        let mut output = String::new();
+
+        Numbered {
+            inner: &mut output,
+            ind: None,
+            started: false,
+        }
+        .write_str(input)
+        .unwrap();
+
+        println!("{}", input);
+        println!("{}", output);
+        println!("{}", expected);
+
+        assert_eq!(expected, output);
     }
 }
