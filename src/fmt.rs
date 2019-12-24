ba0f8c99289f7a24f780812b35d224f8ca377904
Jane Lusby <jlusby42@gmail.com>
2019-12-23 18:43:01 -0800

    Apply indent in causes on each new line (#54)

--- src/fmt.rs
+++ src/fmt.rs
@@ -1,6 +1,6 @@
 use crate::chain::Chain;
 use crate::error::ErrorImpl;
-use core::fmt::{self, Debug};
+use core::fmt::{self, Debug, Write};
 
 impl ErrorImpl<()> {
     pub(crate) fn display(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -28,11 +28,16 @@ impl ErrorImpl<()> {
             write!(f, "\n\nCaused by:")?;
             let multiple = cause.source().is_some();
             for (n, error) in Chain::new(cause).enumerate() {
-                write!(f, "\n    ")?;
-                if multiple {
-                    write!(f, "{}: ", n)?;
-                }
-                write!(f, "{}", error)?;
+                writeln!(f)?;
+                write!(
+                    if multiple {
+                        Indented::numbered(f, n)
+                    } else {
+                        Indented::new(f)
+                    },
+                    "{}",
+                    error
+                )?;
             }
         }
 
@@ -53,5 +58,120 @@ impl ErrorImpl<()> {
         }
 
         Ok(())
+    }
+}
+
+struct Indented<'a, D> {
+    inner: &'a mut D,
+    ind: Option<usize>,
+    started: bool,
+}
+
+impl<'a, D> Indented<'a, D> {
+    fn numbered(inner: &'a mut D, ind: usize) -> Self {
+        Self {
+            inner,
+            ind: Some(ind),
+            started: false,
+        }
+    }
+
+    fn new(inner: &'a mut D) -> Self {
+        Self {
+            inner,
+            ind: None,
+            started: false,
+        }
+    }
+}
+
+impl<T> fmt::Write for Indented<'_, T>
+where
+    T: fmt::Write,
+{
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for (ind, mut line) in s.split('\n').enumerate() {
+            if !self.started {
+                // trim first line to ensure it lines up with the number nicely
+                line = line.trim();
+                // Don't render the first line unless its actually got text on it
+                if line.is_empty() {
+                    continue;
+                }
+
+                self.started = true;
+                match self.ind {
+                    Some(ind) => self.inner.write_fmt(format_args!("{: >5}: ", ind))?,
+                    None => self.inner.write_fmt(format_args!("    "))?,
+                }
+            } else if ind > 0 {
+                self.inner.write_char('\n')?;
+                if self.ind.is_some() {
+                    self.inner.write_fmt(format_args!("       "))?;
+                } else {
+                    self.inner.write_fmt(format_args!("    "))?;
+                }
+            }
+
+            self.inner.write_fmt(format_args!("{}", line))?;
+        }
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_digit() {
+        let input = "verify\nthis";
+        let expected = "    2: verify\n       this";
+        let mut output = String::new();
+
+        Indented {
+            inner: &mut output,
+            ind: Some(2),
+            started: false,
+        }
+        .write_str(input)
+        .unwrap();
+
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn two_digits() {
+        let input = "verify\nthis";
+        let expected = "   12: verify\n       this";
+        let mut output = String::new();
+
+        Indented {
+            inner: &mut output,
+            ind: Some(12),
+            started: false,
+        }
+        .write_str(input)
+        .unwrap();
+
+        assert_eq!(expected, output);
+    }
+
+    #[test]
+    fn no_digits() {
+        let input = "verify\nthis";
+        let expected = "    verify\n    this";
+        let mut output = String::new();
+
+        Indented {
+            inner: &mut output,
+            ind: None,
+            started: false,
+        }
+        .write_str(input)
+        .unwrap();
+
+        assert_eq!(expected, output);
     }
 }
