97cff686efc05eba6720bd378e5262bb3dd51b6c
David Tolnay <dtolnay@gmail.com>
2021-11-29 08:13:59 -0800

    Show doc for Ok fn

--- src/lib.rs
+++ src/lib.rs
@@ -602,7 +602,7 @@ pub trait Context<T, E>: context::private::Sealed {
         F: FnOnce() -> C;
 }
 
-/// Equivalent to `Ok::<_, anyhow::Error>(value)`.
+/// Equivalent to Ok::<_, anyhow::Error>(value).
 ///
 /// This simplifies creation of an anyhow::Result in places where type inference
 /// cannot deduce the `E` type of the result &mdash; without needing to write
@@ -620,7 +620,6 @@ pub trait Context<T, E>: context::private::Sealed {
 ///    |         |
 ///    |         consider giving this pattern the explicit type `std::result::Result<i32, E>`, where the type parameter `E` is specified
 /// ```
-#[doc(hidden)]
 #[allow(non_snake_case)]
 pub fn Ok<T>(t: T) -> Result<T> {
     Result::Ok(t)
