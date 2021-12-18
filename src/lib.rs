a3f7160ce86f72a346c8c48b1b8851d3e49a9e0d
David Tolnay <dtolnay@gmail.com>
2021-12-17 18:26:41 -0800

    Ignore return_self_not_must_use clippy lint
        error: missing `#[must_use]` attribute on a method returning `Self`
           --> src/error.rs:296:5
            |
        296 | /     pub fn context<C>(self, context: C) -> Self
        297 | |     where
        298 | |         C: Display + Send + Sync + 'static,
        299 | |     {
        ...   |
        323 | |         unsafe { Error::construct(error, vtable, backtrace) }
        324 | |     }
            | |_____^
            |
            = note: `-D clippy::return-self-not-must-use` implied by `-D clippy::all`
            = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#return_self_not_must_use

--- src/lib.rs
+++ src/lib.rs
@@ -225,6 +225,7 @@
     clippy::needless_doctest_main,
     clippy::new_ret_no_self,
     clippy::redundant_else,
+    clippy::return_self_not_must_use,
     clippy::unused_self,
     clippy::used_underscore_binding,
     clippy::wildcard_imports,
