471d6e9f0229a7c4ee1d70f8182bf2e9e0e1085d
David Tolnay <dtolnay@gmail.com>
2022-02-18 18:18:14 -0800

    Release 1.0.54

--- src/lib.rs
+++ src/lib.rs
@@ -210,7 +210,7 @@
 //! will require an explicit `.map_err(Error::msg)` when working with a
 //! non-Anyhow error type inside a function that returns Anyhow's error type.
 
-#![doc(html_root_url = "https://docs.rs/anyhow/1.0.53")]
+#![doc(html_root_url = "https://docs.rs/anyhow/1.0.54")]
 #![cfg_attr(backtrace, feature(backtrace))]
 #![cfg_attr(doc_cfg, feature(doc_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
