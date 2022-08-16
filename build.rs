b916fa964164b2b01db1c8447102d6104e8dc5cc
David Tolnay <dtolnay@gmail.com>
2022-08-16 13:20:17 -0700

    Revert "Work around RA's RUSTC_WRAPPER poisoning all subsequent command-line builds"
    This reverts commit 77317b50c832032ff18b2fe22747b09a84a70656.
    
    Closes #261.

--- build.rs
+++ build.rs
@@ -65,9 +65,6 @@ fn main() {
 }
 
 fn compile_probe() -> Option<ExitStatus> {
-    // FIXME: obsoleted by https://github.com/rust-lang/cargo/pull/10973
-    println!("cargo:rerun-if-env-changed=RUSTC_WRAPPER");
-
     let rustc = env::var_os("RUSTC")?;
     let out_dir = env::var_os("OUT_DIR")?;
     let probefile = Path::new(&out_dir).join("probe.rs");
