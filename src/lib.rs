e436660f3e09c639383a39fe7b6a0156b761db0b
laralove <laralove143@icloud.com>
2022-11-06 18:49:15 +0300

    Update backtrace documentation for Rust 1.65 release

--- src/lib.rs
+++ src/lib.rs
@@ -128,11 +128,11 @@
 //!   # ;
 //!   ```
 //!
-//! - If using the nightly channel, or stable with `features = ["backtrace"]`, a
-//!   backtrace is captured and printed with the error if the underlying error
-//!   type does not already provide its own. In order to see backtraces, they
-//!   must be enabled through the environment variables described in
-//!   [`std::backtrace`]:
+//! - If using Rust 1.65 or above, or an older version with
+//!   `features = ["backtrace"]`, a backtrace is captured and printed with the
+//!   error if the underlying error type does not already provide its own. In order
+//!   to see backtraces, they must be enabled through the environment variables
+//!   described in [`std::backtrace`]:
 //!
 //!   - If you want panics and errors to both have backtraces, set
 //!     `RUST_BACKTRACE=1`;
@@ -140,10 +140,7 @@
 //!   - If you want only panics to have backtraces, set `RUST_BACKTRACE=1` and
 //!     `RUST_LIB_BACKTRACE=0`.
 //!
-//!   The tracking issue for this feature is [rust-lang/rust#53487].
-//!
 //!   [`std::backtrace`]: https://doc.rust-lang.org/std/backtrace/index.html#environment-variables
-//!   [rust-lang/rust#53487]: https://github.com/rust-lang/rust/issues/53487
 //!
 //! - Anyhow works with any error type that has an impl of `std::error::Error`,
 //!   including ones defined in your crate. We do not bundle a `derive(Error)`
