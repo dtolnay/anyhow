0798a9a635edc46dd80a09b77f3238eb0d5fba7a
David Tolnay <dtolnay@gmail.com>
2023-08-15 08:25:05 -0700

    Reword bootstrap comment

--- build.rs
+++ build.rs
@@ -69,10 +69,13 @@ fn main() {
 
 fn compile_probe() -> Option<ExitStatus> {
     if env::var_os("RUSTC_STAGE").is_some() {
-        // We are running inside rustc bootstrap. This is a highly non-standard environment with
-        // issues such as <https://github.com/rust-lang/cargo/issues/11138> and
-        // <https://github.com/rust-lang/rust/issues/114839>. Let's just not use nightly features
-        // here.
+        // We are running inside rustc bootstrap. This is a highly non-standard
+        // environment with issues such as:
+        //
+        //     https://github.com/rust-lang/cargo/issues/11138
+        //     https://github.com/rust-lang/rust/issues/114839
+        //
+        // Let's just not use nightly features here.
         return None;
     }
 
