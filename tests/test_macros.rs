f885a133ede579c45e90ab489455126403d05db1
David Tolnay <dtolnay@gmail.com>
2024-02-09 18:52:51 -0800

    Ignore incompatible_msrv clippy false positives in test
    https://github.com/rust-lang/rust-clippy/issues/12257
    
        warning: current MSRV (Minimum Supported Rust Version) is `1.39.0` but this item is stable since `1.64.0`
          --> tests/test_macros.rs:64:39
           |
        64 |         future::ready(anyhow!("...")).await;
           |                                       ^^^^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#incompatible_msrv
           = note: `-W clippy::incompatible-msrv` implied by `-W clippy::all`
           = help: to override `-W clippy::all` add `#[allow(clippy::incompatible_msrv)]`
    
        warning: current MSRV (Minimum Supported Rust Version) is `1.39.0` but this item is stable since `1.48.0`
          --> tests/test_macros.rs:64:9
           |
        64 |         future::ready(anyhow!("...")).await;
           |         ^^^^^^^^^^^^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#incompatible_msrv
    
        warning: current MSRV (Minimum Supported Rust Version) is `1.39.0` but this item is stable since `1.64.0`
          --> tests/test_macros.rs:72:59
           |
        72 |         future::ready(anyhow!(message(Cell::new("...")))).await;
           |                                                           ^^^^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#incompatible_msrv
    
        warning: current MSRV (Minimum Supported Rust Version) is `1.39.0` but this item is stable since `1.48.0`
          --> tests/test_macros.rs:72:9
           |
        72 |         future::ready(anyhow!(message(Cell::new("...")))).await;
           |         ^^^^^^^^^^^^^
           |
           = help: for further information visit https://rust-lang.github.io/rust-clippy/master/index.html#incompatible_msrv

--- tests/test_macros.rs
+++ tests/test_macros.rs
@@ -1,6 +1,7 @@
 #![allow(
     clippy::assertions_on_result_states,
     clippy::eq_op,
+    clippy::incompatible_msrv, // https://github.com/rust-lang/rust-clippy/issues/12257
     clippy::items_after_statements,
     clippy::needless_pass_by_value,
     clippy::shadow_unrelated,
