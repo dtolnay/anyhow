85f0a69907645f04365d1584e7fe00378ec55c7f
David Tolnay <dtolnay@gmail.com>
2024-05-17 10:35:38 -0700

    Add ensure test with a Deref to bool

--- tests/ui/ensure-nonbool.rs
+++ tests/ui/ensure-nonbool.rs
@@ -1,13 +1,28 @@
 use anyhow::{ensure, Result};
+use std::ops::Deref;
+
+struct Bool(bool);
+
+struct DerefBool(bool);
+
+impl Deref for DerefBool {
+    type Target = bool;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 fn main() -> Result<()> {
     ensure!("...");
 
-    struct Struct(bool);
-    let mut s = Struct(true);
+    let mut s = Bool(true);
     match &mut s {
-        Struct(cond) => ensure!(cond),
+        Bool(cond) => ensure!(cond),
     }
+
+    let db = DerefBool(true);
+    ensure!(db);
+    ensure!(&db);
 
     Ok(())
 }
