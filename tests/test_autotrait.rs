a85e414bf555ed961f59c2c69410e7b5f706df65
David Tolnay <dtolnay@gmail.com>
2024-09-14 19:31:49 -0700

    Add more autotraits tests

--- tests/test_autotrait.rs
+++ tests/test_autotrait.rs
@@ -1,6 +1,7 @@
 #![allow(clippy::extra_unused_type_parameters)]
 
 use anyhow::Error;
+use std::panic::{RefUnwindSafe, UnwindSafe};
 
 #[test]
 fn test_send() {
@@ -12,4 +13,22 @@ fn test_send() {
 fn test_sync() {
     fn assert_sync<T: Sync>() {}
     assert_sync::<Error>();
+}
+
+#[test]
+fn test_unwind_safe() {
+    fn assert_unwind_safe<T: UnwindSafe>() {}
+    assert_unwind_safe::<Error>();
+}
+
+#[test]
+fn test_ref_unwind_safe() {
+    fn assert_ref_unwind_safe<T: RefUnwindSafe>() {}
+    assert_ref_unwind_safe::<Error>();
+}
+
+#[test]
+fn test_unpin() {
+    fn assert_unpin<T: Unpin>() {}
+    assert_unpin::<Error>();
 }
